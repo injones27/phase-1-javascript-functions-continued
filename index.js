10209d4ff35e1f4abf3156978d3a8a8bfbcaff62
Isaiah Jones <injones27@gmail.com>
2023-04-16 21:10:54 -0400

    Phase-1 Functions Lab (Continued) Completed!

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (verb = 'roller-skate') {
+    return `This Saturday, I want to ${verb}!`
+}
+
+function mondayWork (travel = 'go to the office') {
+    return `This Monday, I will ${travel}.`
+}
+
+function wrapAdjective(astrx = "*") {
+    return function (adj = "a hard worker") {
+            return `You are ${astrx}${adj}${astrx}!`
+    }
+}
